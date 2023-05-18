7066498483c6647a6a69dd004e0ccd54ea866a56
Leooo <VanLocBT98>
2023-05-18 17:50:07 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,68 +1,181 @@
-import { useEffect, useState } from "react";
-import './App.css'
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+import "./App.css";
+
+gsap.registerPlugin(MotionPathPlugin);
 
 function App() {
-  const [hasLoadedRephael, setHasLoadedPhael] = useState(false);
-  const [hasLoadedWheelNav, setHasLoadedWheelNav] = useState(false);
-
-  const appendScript = () => {
-    const script = document.createElement("script");
-    script.id = "raphael";
-    script.src = "wheelnav/raphael.min.js";
-    script.async = true;
-    script.defer = true;
-    script.crossOrigin = "anonymous";
-    script.onload = () => setHasLoadedPhael(true);
-    document.body.append(script);
-  };
-
-  const scriptAlreadyExists = () => {
-    return (
-      document.querySelector("script#raphael") !== null &&
-      document.querySelector("script#wheelnav") !== null
-    );
-  };
+  const circlePathRef = useRef(null);
+  const itemsRef = useRef([]);
 
   useEffect(() => {
-    if (!scriptAlreadyExists()) {
-      appendScript();
-      appendWheelnavScript();
+    const circlePath = MotionPathPlugin.convertToPath(circlePathRef.current, false)[0];
+    circlePath.id = "circlePath";
+    document.querySelector("svg").prepend(circlePath);
+
+    const items = itemsRef.current;
+    const numItems = items.length;
+    const itemStep = 1 / numItems;
+    const wrapProgress = gsap.utils.wrap(0, 1);
+    const snap = gsap.utils.snap(itemStep);
+    const wrapTracker = gsap.utils.wrap(0, numItems);
+    const tracker = { item: 0 };
+
+    gsap.set(items, {
+      motionPath: {
+        path: circlePath,
+        align: circlePath,
+        alignOrigin: [0.5, 0.5],
+        end: (i) => i / items.length,
+      },
+      scale: 0.9,
+    });
+
+    const tl = gsap.timeline({ paused: true, reversed: true });
+
+    tl.to(".wrapper", {
+      rotation: 360,
+      transformOrigin: "center",
+      duration: 1,
+      ease: "none",
+    });
+
+    tl.to(
+      items,
+      {
+        rotation: "-=360",
+        transformOrigin: "center center",
+        duration: 1,
+        ease: "none",
+      },
+      0
+    );
+
+    tl.to(
+      tracker,
+      {
+        item: numItems,
+        duration: 1,
+        ease: "none",
+        modifiers: {
+          item: (value) => wrapTracker(numItems - Math.round(value)),
+        },
+      },
+      0
+    );
+
+    items.forEach(function (el, i) {
+      el.addEventListener("click", function () {
+        var current = tracker.item,
+          activeItem = i;
+
+        if (i === current) {
+          return;
+        }
+
+        document.querySelector(".item.active").classList.remove("active");
+        items[activeItem].classList.add("active");
+
+        var diff = current - i;
+
+        if (Math.abs(diff) < numItems / 2) {
+          moveWheel(diff * itemStep);
+        } else {
+          var amt = numItems - Math.abs(diff);
+
+          if (current > i) {
+            moveWheel(amt * -itemStep);
+          } else {
+            moveWheel(amt * itemStep);
+          }
+        }
+      });
+    });
+
+    document.getElementById("next").addEventListener("click", function () {
+      var i = -1;
+      var currentIndex = tracker.item;
+
+      if (i === 0) {
+        console.log(items[currentIndex]);
+      } else if (i < 0) {
+        console.log(
+          items[(currentIndex + items.length + i) % items.length]
+        );
+      } else if (i > 0) {
+        console.log(items[(currentIndex + i) % items.length]);
+      }
+
+      return moveWheel(-itemStep);
+    });
+
+    document.getElementById("prev").addEventListener("click", function () {
+      return moveWheel(itemStep);
+    });
+
+    function moveWheel(amount) {
+      let progress = tl.progress();
+      tl.progress(wrapProgress(snap(tl.progress() + amount)));
+      let next = tracker.item;
+      tl.progress(progress);
+
+      document.querySelector(".item.active").classList.remove("active");
+      items[next].classList.add("active");
+
+      gsap.to(tl, {
+        progress: snap(tl.progress() + amount),
+        modifiers: {
+          progress: wrapProgress,
+        },
+      });
     }
   }, []);
 
-  const appendWheelnavScript = () => {
-    const script = document.createElement("script");
-    script.id = "wheelnav";
-    script.src = "wheelnav/wheelnav.js";
-    script.async = true;
-    script.defer = true;
-    script.crossOrigin = "anonymous";
-    script.onload = () => setHasLoadedWheelNav(true);
-    document.body.append(script);
-  };
-
-  useEffect(() => {
-    if (hasLoadedWheelNav && hasLoadedRephael) {
-      // This demo use two wheels on each other
-      let wheel = new window.wheelnav("piemenu");
-      wheel.clockwise = false;
-      wheel.wheelRadius = wheel.wheelRadius * 0.83;
-      wheel.createWheel();
-    }
-  }, [hasLoadedWheelNav, hasLoadedRephael]);
   return (
-    <div className="App">
-     <div id='piemenu' data-wheelnav
- data-wheelnav-slicepath='DonutSlice'
- data-wheelnav-navangle='180'
- data-wheelnav-cssmode 
- data-wheelnav-init>
-  <div data-wheelnav-navitemtext='0' onmouseup='alert("Place your logic here.");'></div>
-  <div data-wheelnav-navitemtext='1' onmouseup='alert("Place your logic here.");'></div>
-  <div data-wheelnav-navitemtext='2' onmouseup='alert("Place your logic here.");'></div>
-  <div data-wheelnav-navitemtext='3' onmouseup='alert("Place your logic here.");'></div>
-  <div data-wheelnav-navitemtext='4' onmouseup='alert("Place your logic here.");'></div>
-</div>
+    <div className="container">
+      <div className="wrapper">
+        <div className="item active" ref={(el) => (itemsRef.current[0] = el)}>
+          1
+        </div>
+        <div className="item" ref={(el) => (itemsRef.current[1] = el)}>
+          2
+        </div>
+        <div className="item" ref={(el) => (itemsRef.current[2] = el)}>
+          3
+        </div>
+        <div className="item" ref={(el) => (itemsRef.current[3] = el)}>
+          4
+        </div>
+        <div className="item" ref={(el) => (itemsRef.current[4] = el)}>
+          5
+        </div>
+        <div className="item" ref={(el) => (itemsRef.current[5] = el)}>
+          6
+        </div>
+        <div className="item" ref={(el) => (itemsRef.current[6] = el)}>
+          7
+        </div>
+        <div className="item" ref={(el) => (itemsRef.current[7] = el)}>
+          8
+        </div>
+        <svg viewBox="0 0 300 300">
+          <circle
+            id="holder"
+            className="st0"
+            cx="151"
+            cy="151"
+            r="150"
+            ref={circlePathRef}
+          />
+        </svg>
+      </div>
+      <div className="start">&#8592; Active</div>
+      <div className="container" style={{ textAlign: "center" }}>
+        <button id="prev">Prev</button>
+        <button id="next">Next</button>
+      </div>
     </div>
   );
 }
